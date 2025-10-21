36766b0596375217b2caf9cae546f0bdb618a81b
josephmullins <josephlyonmullins@gmail.com>
2025-10-21 14:42:19 -0500

    speed test

---
+++ speed_test.jl
@@ -0,0 +1,10 @@
+include("parameters.jl")
+include("estimation.jl")
+include("inference.jl")
+using LinearAlgebra, Distributions, DelimitedFiles
+
+p = default_model()
+
+N = 1_000
+monte_carlo_simulation_threaded(p; N, nboot = 1)
+@time monte_carlo_simulation_threaded(p; N, nboot = 1_000)
