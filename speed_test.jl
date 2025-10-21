deaaae7511f3a615b5f9057c0a4ab72d7c207e85
josephmullins <josephlyonmullins@gmail.com>
2025-10-21 14:42:47 -0500

    #

--- speed_test.jl
+++ speed_test.jl
@@ -6,5 +6,5 @@ using LinearAlgebra, Distributions, DelimitedFiles
 p = default_model()
 
 N = 1_000
-monte_carlo_simulation_threaded(p; N, nboot = 1)
+monte_carlo_simulation_threaded(p; N, nboot = Threads.nthreads())
 @time monte_carlo_simulation_threaded(p; N, nboot = 1_000)
